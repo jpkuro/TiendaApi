1dfea49140662ede053c34ea92e4d62bc8bca004
jpkuro <joseluispalmapiguave@gmail.com>
2022-10-06 23:52:41 -0500

    se agrego consumitr api

---
+++ ConsumirApi/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ConsumirApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net;
+using TiendaApi.Modelo;
+
+namespace ConsumirApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://localhost:7093/api/productos");
+            var productolista = JsonConvert.DeserializeObject<List<ModeloProductos>>(json);
+            return View(productolista);
+            //WebRequest oRequest = WebRequest.Create("https://localhost:7093/api/productos");
+            // WebResponse oResponse = oRequest.GetResponse();
+            // StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            //return await sr.ReadToEndAsync();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ pedirDatos/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace pedirDatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private async Task<string> gethttp()
+        {
+            WebRequest oRequest = WebRequest.Create("https://localhost:7093/api/productos");
+            WebResponse oResponse = oRequest.GetResponse();
+            StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            return await sr.ReadToEndAsync();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string respuesta = await gethttp();
+            
+        }
+    }
+}
